fb4f4749c31b182d40c4a6ecdfc87b876fa7215c
fmatecny <xmatec00@stud.fit.vutbr.cz>
2014-11-24 14:56:40 +0100

    Create parser.c

---
+++ parser.c
@@ -0,0 +1,9 @@
+/*
+* Projekt do IFJ - interpret jazyka IFJ13
+* Soubor: interpret.c
+* Autoři: František Matečný (xmatec00),
+* Adam Novák (xnovak1b),
+* Jakub Kačina,
+* Matej Pecho,
+* Lukáš Mandáček
+*/
